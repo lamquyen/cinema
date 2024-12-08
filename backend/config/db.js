a9d738de8d59b694d9b7bb04e399825e95ceaa19
VP17-1752046 <luongtin8@gmail.com>
2024-12-08 17:49:45 +0700

    config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-async function connect() {
+export async function connect() {
   try {
     await mongoose.connect(process.env.DATABASE_URL);
     console.log("connect successfuly!!!");
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
 import userRouter from "./Routes/UserRouter.js";
 import { errorHandler } from "./middlewares/errorMiddleware.js";
+import { connect } from "./config/db.js";
 
 dotenv.config();
 
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // connect DB
-connectDB();
+connect();
 
 app.get("/", (req, res) => {
   res.send("API is running ...");
